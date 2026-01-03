4b652d086498c1deba08cb6aef5fc5b337ae74bf
Bruno Desktop <carecsnay1@gmail.com>
2026-01-03 20:07:51 -0300

    ✨ (disney.js): fetch disney characters from api ✨ (disney.js): render characters with images and films ✨ (disney.js): add page title and header ✨ (disney.js): use react bootstrap for styling

--- pages/disney.js
+++ pages/disney.js
@@ -1,11 +1,48 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import Head from 'next/head'
+import React, { useEffect, useState } from 'react'
+import { Card, Col, Container, Row } from 'react-bootstrap'
 
 const disney = () => {
     const [disneyData, setDisneyData] = useState([])
 
+    useEffect(() => {
+        axios.get('https://api.disneyapi.dev/character').then(response => {
+            setDisneyData(response.data.data)
+        })
+    }, [])
 
     return (
-        <h1>Disney API</h1>
+        <>
+            <Head>
+                <title>Disney API</title>
+            </Head>
+
+            <div className='text-center my-5'>
+                <h1>Disney API</h1>
+                <p>Consumo da API da Disney utilizando hooks do react</p>
+            </div>
+            <Row sm={1} md={2} lg={3} xxl={4}>
+                {disneyData.map((character =>
+                    <Col key={character._id} className='mb-3'>
+                        <Card className="h-100">
+                            <Card.Img variant="top" src={character.imageUrl} height={'200px'} />
+                            <Card.Body className='p-3'>
+                                <Card.Title className='text-center'>{character.name}</Card.Title>
+                                <Card.Text >
+                                    <p className='text-center fw-bold'>Visto em:</p>
+                                    <ul>
+                                        {character.films.map(filme =>
+                                            <li key={filme}>{filme}</li>
+                                        )}
+                                    </ul>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
